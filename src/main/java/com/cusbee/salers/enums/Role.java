2a85e7f2ada570d35cc2f9121c7e63b09140bd18
Paul Luchyn <Paul Luchyn>
2016-07-11 14:00:45 +0300

    refactor and adding spring security

--- src/main/java/com/cusbee/salers/enums/Role.java
+++ src/main/java/com/cusbee/salers/enums/Role.java
@@ -2,16 +2,7 @@ package com.cusbee.salers.enums;
 
 public enum Role {
 
-    /*0*/ROLE_SALER("Saler"),
-    /*1*/ROLE_ADMIN("Admin");
+    ROLE_SALER,
+    ROLE_ADMIN;
 
-    private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    Role(String description) {
-        this.description = description;
-    }
 }
