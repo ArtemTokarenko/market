c8db64664d435c04580469f5f77f6e4453f04fcc
Paul Luchyn <Paul Luchyn>
2016-07-13 15:37:03 +0300

    import optimization

--- src/main/java/com/cusbee/salers/controller/UpworkBidController.java
+++ src/main/java/com/cusbee/salers/controller/UpworkBidController.java
@@ -6,7 +6,6 @@ import com.cusbee.salers.service.interfaces.UpworkBidService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
--- src/main/java/com/cusbee/salers/service/interfaces/UpworkBidService.java
+++ src/main/java/com/cusbee/salers/service/interfaces/UpworkBidService.java
@@ -3,7 +3,6 @@ package com.cusbee.salers.service.interfaces;
 import com.cusbee.salers.beans.UpworkBidBean;
 import com.cusbee.salers.entity.UpworkBid;
 
-import java.util.Date;
 import java.util.List;
 
 public interface UpworkBidService {
